cff067249ecb7378918a6ce997e5616960321485
David Serrano <dserrano@visiotechsecurity.com>
2023-12-24 11:15:03 +0100

    [FEAT] Añadido 3.1

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+let contacts = [
+    {
+        id: 1,
+        name: 'John Doe',
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: 'Ada Lovelace',
+        number: "39-00-532523"
+    },
+    {
+        id: 3,
+        name: 'Dan Abramov',
+        number: "12-43-23445"
+    },
+    {
+        id: 4,
+        name: 'Mary Poppendick',
+        number: "39-23-6423122"
+    },
+]
+
+app.get('/', (request, response) => {
+    response.send("OK")
+})
+
+app.get('/api/persons', (request, response) => {
+    response.send(contacts)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
